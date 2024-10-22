f774189743c7f7c14f17ddbfaea016c5fa3e4300
techfullrenew <techfullrenew@gmail.com>
2024-10-22 15:19:33 +0000

    feat: Adding sections to webpage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'logo_slide': 'logo_slide 30s linear infinite',
+			},
+			keyframes: {
+				logo_slide: {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(-100%)' },
+				}
+			}
+		},
 	},
 	plugins: [],
 }
