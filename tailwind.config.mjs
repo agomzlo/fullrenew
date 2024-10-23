76fdc4b36f5d522e0f7b485450e0c972af10d414
techfullrenew <techfullrenew@gmail.com>
2024-10-23 16:04:04 +0000

    feat: Adding more sections and refactor another one

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,12 +5,18 @@ export default {
 		extend: {
 			animation: {
 				'logo_slide': 'logo_slide 30s linear infinite',
+				'images_slide': 'images_slide 30s linear infinite',
+				'images_slide_reverse': 'images_slide 30s linear infinite reverse'
 			},
 			keyframes: {
 				logo_slide: {
 					'0%': { transform: 'translateX(0)' },
 					'100%': { transform: 'translateX(-100%)' },
-				}
+				},
+				images_slide: {
+					'0%': { transform: 'translateY(0)' },
+					'100%': { transform: 'translateY(-100%)' },
+				},
 			}
 		},
 	},
