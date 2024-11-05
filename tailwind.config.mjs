2c9e9831f3a3c9d967e426cecc5afc6602cdbafc
techfullrenew <techfullrenew@gmail.com>
2024-11-05 12:54:29 +0000

    feat: Doing the page responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,15 +3,20 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				'background': '#171717',
+				'primary': '#FFFFFF',
+				'secondary': '#d4d4d4',
+				'whatsapp': '#25D366'
+			},
 			animation: {
-				'logo_slide': 'logo_slide 30s linear infinite',
+				'logo_slide': 'logo_slide 5s linear infinite',
 				'images_slide': 'images_slide 30s linear infinite',
 				'images_slide_reverse': 'images_slide 30s linear infinite reverse'
 			},
 			keyframes: {
 				logo_slide: {
-					'0%': { transform: 'translateX(0)' },
-					'100%': { transform: 'translateX(-100%)' },
+					'to': { transform: 'translateX(-314px)' }
 				},
 				images_slide: {
 					'0%': { transform: 'translateY(0)' },
