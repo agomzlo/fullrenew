baff6fc980caefccc6ab8084ed57947a39c6486a
techfullrenew <techfullrenew@gmail.com>
2024-11-13 11:56:46 +0000

    feat: Changing library to send emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   integrations: [tailwind()]
 });
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.PUBLIC_RESEND_API_KEY)
+
+export const POST: APIRoute = async ({ request }) => {
+    try{
+        const { name, phonePrefix, phone, email, company_name, subject, message } = await request.json();
+
+        await resend.emails.send({
+            from: 'fullRenew <onboarding@resend.dev>',
+            to: 'agomzlo11@gmail.com',
+            subject: 'Una nueva empresa se quiere poner en contacto con nosotros',
+            html: `<p><strong>Nombre:</strong> ${name}</p>
+                  <p><strong>Teléfono:</strong> ${phonePrefix} ${phone}</p>
+                  <p><strong>Email:</strong> ${email}</p>
+                  <p><strong>Empresa:</strong> ${company_name}</p>
+                  <p><strong>Asunto:</strong> ${subject}</p>
+                  <p><strong>Mensaje:</strong></p>
+                  <p>${message}</p>`,
+        })
+        return new Response(null, {status: 200})
+    }catch{
+        return new Response(null, {status: 500})
+    }
+}
